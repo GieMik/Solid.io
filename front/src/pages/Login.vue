52f584b6843ede8239db880d350722913dbc0ec0
Giedrius Mikoliūnas <giemik@gmail.com>
2017-07-16 21:18:12 +0300

    Create login & Register forms

--- front/src/pages/Login.vue
+++ front/src/pages/Login.vue
@@ -1,3 +1,49 @@
 <template>
-	<h1>This is Login page</h1>
+	<div class="login-page-wrapper">
+		<div class="login-form form-md form-wrapper">
+			<div class="form-header">
+				<h2 class="form-title">Please login..</h2>
+			</div>
+			<div class="form-body">
+				<div class="f-field">
+					<label class="label" for="email">Email</label>
+					<input
+						v-validate="'required|email'"
+						:class="{'input': true, 'is-danger': errors.has('email') }"
+						name="email"
+						type="text"
+						placeholder="Email">
+					<span
+						v-show="errors.has('email')"
+						class="help is-danger">{{ errors.first('email') }}</span>
+				</div>
+				<div class="f-field">
+					<label class="label" for="password">Password</label>
+					<input
+						v-validate="'required|password'"
+						:class="{'input': true, 'is-danger': errors.has('password') }"
+						name="password"
+						type="password"
+						placeholder="Password">
+					<span
+						v-show="errors.has('password')"
+						class="help is-danger">{{ errors.first('password') }}</span>
+				</div>
+			</div>
+			<div class="form-footer">
+				<button class="btn">Login</button>
+			</div>
+		</div>
+	</div>
 </template>
+
+<script>
+	export default {
+		data() {
+			return {
+				email: '',
+				password: ''
+			}
+		}
+	}
+</script>
--- front/src/pages/Register.vue
+++ front/src/pages/Register.vue
@@ -1,3 +1,62 @@
 <template>
-	<h1>Register page</h1>
+	<div class="login-page-wrapper">
+		<div class="login-form form-md form-wrapper">
+			<div class="form-header">
+				<h2 class="form-title">Fill The Form..</h2>
+			</div>
+			<div class="form-body">
+				<div class="f-field">
+					<label class="label" for="email">Email</label>
+					<input
+						v-validate="'required|email'"
+						:class="{'input': true, 'is-danger': errors.has('email') }"
+						name="email"
+						type="text"
+						placeholder="Email">
+					<span
+						v-show="errors.has('email')"
+						class="help is-danger">{{ errors.first('email') }}</span>
+				</div>
+				<div class="f-field">
+					<label class="label" for="password">Password</label>
+					<input
+						v-validate="'required|password'"
+						:class="{'input': true, 'is-danger': errors.has('password') }"
+						name="password"
+						type="password"
+						placeholder="Password">
+					<span
+						v-show="errors.has('password')"
+						class="help is-danger">{{ errors.first('password') }}</span>
+				</div>
+				<div class="f-field">
+					<label class="label" for="repeatPassword">Repeat Password</label>
+					<input
+						v-validate="'required|repeatPassword'"
+						:class="{'input': true, 'is-danger': errors.has('repeatPassword') }"
+						name="repeatPassword"
+						type="password"
+						placeholder="Repeat Password">
+					<span
+						v-show="errors.has('repeatPassword')"
+						class="help is-danger">{{ errors.first('repeatPassword') }}</span>
+				</div>
+			</div>
+			<div class="form-footer">
+				<button class="btn">Register</button>
+			</div>
+		</div>
+	</div>
 </template>
+
+<script>
+	export default {
+		data() {
+			return {
+				email: '',
+				password: '',
+				repeatPassword: ''
+			}
+		}
+	}
+</script>

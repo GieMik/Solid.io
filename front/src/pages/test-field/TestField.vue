d7ecbed750814d08dd57b6091e20c44428baaaa1
Giedrius Mikoliūnas <giemik@gmail.com>
2017-07-17 21:36:50 +0300

    Update test field page & some other fixes

--- front/src/pages/test-field/TestField.vue
+++ front/src/pages/test-field/TestField.vue
@@ -1,3 +1,44 @@
 <template>
-	<router-view></router-view>
+	<div>
+		<h2>This is test field</h2>
+		<ol>
+			<li>Containers</li>
+			<li>Responsive Breakpoints</li>
+			<li>Grid System</li>
+			<li>Alignment</li>
+			<li>Headings</li>
+			<li>Paragraphs</li>
+			<li>Lists</li>
+			<li>Tables</li>
+			<li>Forms</li>
+			<li>Blockquote</li>
+			<li>Inline text elements</li>
+			<li>Text utilities</li>
+			<li>Alerts</li>
+			<li>Badge</li>
+			<li>Breadcrumb</li>
+			<li>Buttons</li>
+			<li>Button Group</li>
+			<li>Card</li>
+			<li>Carousel</li>
+			<li>Collapse</li>
+			<li>Dropdowns</li>
+			<li>Forms</li>
+			<li>Input Group</li>
+			<li>List Group</li>
+			<li>Modal</li>
+			<li>Navs</li>
+			<li>Navbar</li>
+			<li>Pagination</li>
+			<li>Popovers</li>
+			<li>Progress</li>
+			<li>Scrollspy</li>
+			<li>Borders</li>
+			<li>Clearfix</li>
+			<li>Close Icon</li>
+			<li>Colors</li>
+			<li>Flexbox</li>
+			<li>Spacing</li>
+		</ol>
+	</div>
 </template>
